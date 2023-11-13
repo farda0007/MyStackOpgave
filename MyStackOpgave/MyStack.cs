0427bde3a3b45a9226e746534030eb5b73a9b37a
farda0007 <ardaumutyildiz@gmail.com>
2023-11-13 09:52:39 +0100

    Add project files.

---
+++ MyStackOpgave/MyStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MyStackOpgave
+{
+    public class MyStack
+    {
+        private int top = 0;
+        private int[] stack = new int[5];
+
+        public void Push(int element)
+        {
+            if (top > 5)
+            {
+                Console.WriteLine("Stack overflow");
+            }
+            else
+            {
+                Push(element);
+                top++; 
+               
+            }
+        }
+
+        public int Pop(int element)
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+                top--; Pop(element);
+            return element;
+        }
+    }
+}
+
---
+++ MyStackOpgave/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using MyStackOpgave;
+
+
+MyStack stack = new MyStack();
+stack.Push(4);
+stack.Push(5);
+
